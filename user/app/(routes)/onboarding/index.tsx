75b044726673dc124d8bdce193cca6ce24863f28
duowngng <duowng.ng@gmail.com>
2024-11-15 20:57:28 +0700

    Onboarding

---
+++ user/app/(routes)/onboarding/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import OnboardingScreen from "@/screens/onboarding.screen";
+
+export default function Index() {
+    return (
+        <OnboardingScreen />
+    )
+}
