ae49f66c6059073b95d23d57cbba4be153c07872
duowngng <duowng.ng@gmail.com>
2024-11-17 13:07:18 +0700

    Verify Email OTP

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -14,9 +14,9 @@ const userRouter = express.Router();
 userRouter.post("/registration", registerUser);
 
 userRouter.post("/verify-otp", verifyOtp);
-//
+
 userRouter.post("/email-otp-request", sendingOtpToEmail);
-//
+
 userRouter.put("/email-otp-verify", verifyingEmail);
 //
 // userRouter.get("/me", isAuthenticated, getLoggedInUserData);
