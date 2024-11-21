b2ead6b468f067aca899c4e1d96faf75883d8709
duowngng <duowng.ng@gmail.com>
2024-11-21 22:11:16 +0700

    Add driver auth BE

--- server/app.ts
+++ server/app.ts
@@ -2,8 +2,8 @@ require("dotenv").config();
 import express, { NextFunction, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route";
+import driverRouter from "./routes/driver.route";
 import Nylas from "nylas";
-// import driverRouter from "./routes/driver.route";
 
 export const app = express();
 
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 // routes
 app.use("/api/v1", userRouter);
-// app.use("/api/v1/driver", driverRouter);
+app.use("/api/v1/driver", driverRouter);
 
 // testing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
--- user/screens/otp-verification/otp-verification.screen.tsx
+++ user/screens/otp-verification/otp-verification.screen.tsx
@@ -49,13 +49,37 @@ export default function OtpVerificationScreen() {
                         router.push("/(tabs)/home");
                     }
                 })
-                .catch((error) => {
+                .catch(function (error) {
+                    if (error.response) {
+                        // The request was made and the server responded with a status code
+                        // that falls out of the range of 2xx
+                        console.log(error.response.data);
+                        console.log(error.response.status);
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        // The request was made but no response was received
+                        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                        // http.ClientRequest in node.js
+                        console.log(error.request);
+                    } else {
+                        // Something happened in setting up the request that triggered an Error
+                        console.log('Error', error.message);
+                    }
+                    console.log(error.config);
                     setLoading(false);
                     toast.show("Something went wrong! Please check your OTP!", {
                         type: "danger",
                         placement: "bottom",
                     });
                 });
+                // .catch((error) => {
+                //     setLoading(false);
+                //     console.log(error);
+                //     toast.show("Something went wrong! Please check your OTP!", {
+                //         type: "danger",
+                //         placement: "bottom",
+                //     });
+                // });
         }
     };
 
